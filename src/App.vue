eff9c99295abe194c5cfa78fcd934e43cae45701
Jon William Taylor <jwtaylor659@gmail.com>
2023-12-19 16:20:04 -0600

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <QuoteGenerator />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import QuoteGenerator from './components/QuoteGenerator.vue'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    QuoteGenerator
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
